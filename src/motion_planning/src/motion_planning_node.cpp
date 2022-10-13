fa308b1c46e27b74a404496fdadaf42e7d84086c
madspete <madsp17@student.sdu.dk>
2022-10-13 12:23:34 +0200

    Remove unused dependencies

--- src/motion_planning/src/motion_planning_node.cpp
+++ src/motion_planning/src/motion_planning_node.cpp
@@ -3,13 +3,6 @@
 #include <ros/ros.h>
 
 // MoveIt
-#include <moveit/robot_model_loader/robot_model_loader.h>
-#include <moveit/planning_interface/planning_interface.h>
-#include <moveit/planning_scene/planning_scene.h>
-#include <moveit/kinematic_constraints/utils.h>
-#include <moveit_msgs/DisplayTrajectory.h>
-#include <moveit_msgs/PlanningScene.h>
-#include <moveit_visual_tools/moveit_visual_tools.h>
 #include <moveit/move_group_interface/move_group_interface.h>
 
 int main(int argc, char** argv)
